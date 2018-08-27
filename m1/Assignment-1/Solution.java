53dfa56fa768d68f45de4209109c48973eb0b584
saad181 <saadahmed181@msitprogram.net>
2018-08-27 19:29:48 +0530

    Solution.java

---
+++ m1/Assignment-1/Solution.java
@@ -0,0 +1,8 @@
+public class Solution {
+	public static void main(String[] args) {
+		Str name = saad;
+		int roll number = 20186043;
+
+		System.out.println("Name :" +name , "Roll Number : " +roll number);
+	}
+}
