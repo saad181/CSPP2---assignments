3ae6b2ef632f23b2b638c0f5c97e38be0c142eb9
saad181 <saadahmed181@msitprogram.net>
2018-08-27 19:21:08 +0530

    Solution.java

---
+++ m1/Assignment-2/Solution.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Solution {
+	/*
+	Do not modify this main function.
+	*/
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int base = scan.nextInt();
+		int height = scan.nextInt();
+		//areaOfTriangle(base, height);
+		
+		double area = (base*height)/2;
+		System.out.println(+area);
+	}
+	/*
+	Need to fill the areaOfTriangle function and print the output
+	of fahrenheit.
+	*/
+}
