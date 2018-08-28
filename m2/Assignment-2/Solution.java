524f1bc88bdd5fef4200b4ebc5d82561e85eef37
saad181 <saadahmed181@msitprogram.net>
2018-08-28 17:40:57 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m2/Assignment-2/Solution.java
+++ m2/Assignment-2/Solution.java
@@ -2,25 +2,24 @@ import java.util.Scanner;
 import java.lang.Math;
 /**this is a program**/
 public class Solution {
-	/*
-	this is a quadratic equation
-	*/
-	public static void main(String[] args)
-	/**this is used for the scanning purpose**/
-	{
-		Scanner scan = new Scanner(System.in);
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		int c = scan.nextInt();
-		rootsOfQuadraticEquation(a, b, c);
-	}
-	/**Need to write the rootsOfQuadraticEquation function and print the output.**/
-	static void rootsOfQuadraticEquation(int a, int b, int c)
-	{
-		double root1;
-		double root2;
-		root1 = ( - b + (Math.sqrt((b * b) - 4 *a * c))) / (2 * a);
-		root2 = ( - b - (Math.sqrt((b * b) - 4 * a * c))) / (2 * a);
-		System.out.println(root1 +" "+ root2);
-	}
+    /*
+    this is a quadratic equation
+    */
+    public static void main(String[] args) {
+        /**this is used for the scanning purpose**/
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        rootsOfQuadraticEquation(a, b, c);
+    }
+    /**Need to write the rootsOfQuadraticEquation function and print the output.**/
+    static void rootsOfQuadraticEquation(int a, int b, int c)
+    {
+        double root1;
+        double root2;
+        root1 = ( - b + (Math.sqrt((b * b) - 4 *a * c))) / (2 * a);
+        root2 = ( - b - (Math.sqrt((b * b) - 4 * a * c))) / (2 * a);
+        System.out.println(root1 + " " + root2);
+    }
 }
