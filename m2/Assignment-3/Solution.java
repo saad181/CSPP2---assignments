3e0c66acbb13804a2341fd20f15a7ec4a9e956f2
saad181 <saadahmed181@msitprogram.net>
2018-08-28 18:56:09 +0530

    "check_style_score -> 4 of 5 passed. style score: 0/1 "

--- m2/Assignment-3/Solution.java
+++ m2/Assignment-3/Solution.java
@@ -1,28 +1,28 @@
 import java.util.Scanner;
 /**this is a program**/
 final class Solution {
-	/**this is a program**/
-	private Solution() { }
+/**this is a program**/
+    private Solution() { }
 
-	/**@param args this is a string **/
-	public static void main(final String[] args) {
-		/** this is used for scanning purpose*/
-		Scanner s = new Scanner(System.in);
+    /**@param args this is a string **/
+    public static void main(final String[] args) {
+        /** this is used for scanning purpose*/
+        Scanner s = new Scanner(System.in);
         int base = s.nextInt();
         int exponent = s.nextInt();
         int result = power(base, exponent);
         System.out.println(result);
-	}
-	/**
-	@param base variable
+    }
+    /**
+    @param base variable
      @param exponent variable
       Need to write the power function and print the output.
-	**/
-	public static int power(final int base, final int exponent) {
-		if (exponent > 0)
-		{
-			return base * power(base, exponent - 1);
-		}
-		return 1;
-	}
+    **/
+    public static int power(final int base, final int exponent) {
+        if (exponent > 0)
+        {
+            return base * power(base, exponent - 1);
+        }
+        return 1;
+    }
 }
