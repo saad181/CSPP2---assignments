e9252885c210046c610323df6fe6f3279790c5af
saad181 <saadahmed181@msitprogram.net>
2018-08-29 14:50:02 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m2/Assignment-3/Solution.java
+++ m2/Assignment-3/Solution.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
-/**this is a program**/
+/**this is a program.**/
 final class Solution {
-/**this is a program**/
+/**this is a program.**/
     private Solution() { }
 /**@param args this is a string **/
     public static void main(final String[] args) {
-        /** this is used for scanning purpose*/
+        /** this is used for scanning purpose.*/
         Scanner s = new Scanner(System.in);
         long base = s.nextLong();
         long exponent = s.nextLong();
@@ -15,11 +15,11 @@ final class Solution {
     /**
     @param base variable
      @param exponent variable
+     @return value is 1
       Need to write the power function and print the output.
     **/
     public static long power(final long base, final long exponent) {
-        if (exponent > 0)
-        {
+        if (exponent > 0) {
             return (base * power(base, exponent - 1));
         }
         return 1;
