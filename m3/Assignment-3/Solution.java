b6566d1b9b18a18257520798f3af736266ac986b
saad181 <saadahmed181@msitprogram.net>
2018-08-29 18:47:09 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m3/Assignment-3/Solution.java
+++ m3/Assignment-3/Solution.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 /*
 	Do not modify this main function.
 	*/
-public class Solution {
+final class Solution {
+    /** this is a main class**/
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
 
-        Scanner s=new Scanner(System.in);      
+        Scanner s = new Scanner(System.in);      
         int n1 = s.nextInt();
         int n2 = s.nextInt();
         //gcd(n1,n2);
@@ -16,12 +17,13 @@ public class Solution {
     /**
      * { function_description }
      *
-     * @param      n1    The n 1
-     * @param      n2    The n 2
+     * @param      n1    The value of first number
+     * @param      n2    The value of second number
      *
      * @return     { description_of_the_return_value }
      */
-    public static int gcd(int n1, int n2) 
+    public static int gcd(final int n1, final int n2)
+    /** this method is for greatest common divisor*/
     {
         for(int i = n1; i > 1;i --) {
             if(n1 % i == 0){
