9b1d3961ba7ee0187e7f99ac6cf14e6fd387305d
saad181 <saadahmed181@msitprogram.net>
2018-08-30 15:35:12 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m4/Assignment-1/Solution.java
+++ m4/Assignment-1/Solution.java
@@ -8,21 +8,16 @@ public class Solution
 		/** this the main function.**/
 	    Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		
 		int arr[] = new int[n];
 		for (int j = 0; j < n; j++) {
 		    arr[j] = sc.nextInt();
 		}
-		int max = arr[0] ;
-		for (int i = 0; i <arr.length; i++) {
-		    if (arr[i] > max)
-			{
-				max = arr[i];
+		int max = arr[0];
+		for (int i = 0; i < arr.length; i++) {
+		    if (arr[i] > max) {
+			    max = arr[i];
 			}
-			
 		}
 		System.out.println(max);
-		
-
 	}
 }
