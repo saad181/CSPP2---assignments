8e18dc9bfb257f67b0b21ce0dcc374ef91a07da3
saad181 <saadahmed181@msitprogram.net>
2018-08-30 15:39:37 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m4/Assignment-1/Solution.java
+++ m4/Assignment-1/Solution.java
@@ -1,23 +1,25 @@
 import java.util.Scanner;
-public class Solution
-{
-	/** this the main class.**/
-	private Solution() { }
-	/** @param args this is the string.**/
-	public static void main(final String[] args) {
-		/** this the main function.**/
-	    Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int arr[] = new int[n];
-		for (int j = 0; j < n; j++) {
-		    arr[j] = sc.nextInt();
-		}
-		int max = arr[0];
-		for (int i = 0; i < arr.length; i++) {
-		    if (arr[i] > max) {
-			    max = arr[i];
-			}
-		}
-		System.out.println(max);
-	}
+/**
+ * program for largest number
+ */
+final class Solution {
+    /** this the main class.**/
+    private Solution() { }
+    /** @param args this is the string.**/
+    public static void main(final String[] args) {
+        /** this the main function.**/
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int j = 0; j < n; j++) {
+            arr[j] = sc.nextInt();
+        }
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println(max);
+    }
 }
