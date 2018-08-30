fa2fc9f5bf42f33c065b659d448795d1d6d8be50
saad181 <saadahmed181@msitprogram.net>
2018-08-30 18:52:35 +0530

    "check_style_score -> 1 of 4 passed. style score: 0/1 "

--- m4/Assignment-2/Solution.java
+++ m4/Assignment-2/Solution.java
@@ -33,8 +33,8 @@ public class Solution {
 						System.out.println(" ");
 					}
 
-				}System.out.println();
-			}
+				}
+			}System.out.println();
 		}else {
 			System.out.println("not possible");
 		}
