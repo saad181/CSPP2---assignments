33f95e080ad4409345578135f9056ca10ee882dc
saad181 <saadahmed181@msitprogram.net>
2018-08-31 19:51:34 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- m4/Assignment-3/Solution.java
+++ m4/Assignment-3/Solution.java
@@ -1,19 +1,16 @@
 import java.util.Scanner;
 public class Solution
-{/*
-	Do not modify this main function.
-	*/
-	public static void main(final String[] args)
-	{
-		Scanner sc = new Scanner(System.in);
+{
+	public static void main(final String[] args) {
+	    Scanner sc = new Scanner(System.in);
 		int n = Integer.parseInt(sc.nextLine());
-		for(int i = 0; i < n; i++) {
+		for (int i = 0; i < n; i++) {
 			String s = sc.nextLine();
-			String res = binaryToDecimal(s);//Write binaryToDecimal function
+			String res = binaryToDecimal(s);
 			System.out.println(res);
 		}
 	}
-	static String binaryToDecimal(String s) {
+	static String binaryToDecimal(final String s) {
 		int j;
 		int k = 0;
 		for (j = 0; j < s.length(); j++) {
