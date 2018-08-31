50d9e07863b8f669bafee5568ecaf7ded33622c9
saad181 <saadahmed181@msitprogram.net>
2018-08-31 19:57:36 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- m4/Assignment-3/Solution.java
+++ m4/Assignment-3/Solution.java
@@ -4,34 +4,34 @@ final class Solution {
      /** this is a program.**/
      private Solution() { }
      /**@param args this is string.**/
-	public static void main(final String[] args) {
-		/** this is string.**/
-	    Scanner sc = new Scanner(System.in);
-		int n = Integer.parseInt(sc.nextLine());
-		for (int i = 0; i < n; i++) {
-			String s = sc.nextLine();
-			String res = binaryToDecimal(s);
-			System.out.println(res);
-		}
-	}
-	/**
-	 * { function_description }
-	 *
-	 * @param      s     {is a variable }
-	 *
-	 * @return     { k }
-	 */
-	static String binaryToDecimal(final String s) {
-		/** this is the function for binary to decimal.**/
-		int j;
-		int k = 0;
-		for (j = 0; j < s.length(); j++) {
-			if (s.charAt(j) == '1') {
-				k = (int) (k + Math.pow(2, s.length() - 1 - j));
-			}
-		}
-		return k + "";
-	}
-	}
+    public static void main(final String[] args) {
+        /** this is string.**/
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < n; i++) {
+            String s = sc.nextLine();
+            String res = binaryToDecimal(s);
+            System.out.println(res);
+        }
+    }
+    /**
+     * { function_description.}
+     *
+     * @param      s     {is a variable.}
+     *
+     * @return     { k }
+     */
+    static String binaryToDecimal(final String s) {
+        /** this is the function for binary to decimal.**/
+        int j;
+        int k = 0;
+        for (j = 0; j < s.length(); j++) {
+            if (s.charAt(j) == '1') {
+                k = (int) (k + Math.pow(2, s.length() - 1 - j));
+            }
+        }
+        return k + "";
+    }
+    }
 
 
