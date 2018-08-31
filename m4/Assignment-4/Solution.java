9a09c4840794d3de78d7ab633f77fd4b14077ced
saad181 <saadahmed181@msitprogram.net>
2018-08-31 15:45:17 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m4/Assignment-4/Solution.java
+++ m4/Assignment-4/Solution.java
@@ -1,27 +1,25 @@
 import java.util.Scanner;
-public class Solution
-{/*
-	Do not modify the main function 
-	*/
-	public static void main(String[] args)
-	{
-		Scanner sc=new Scanner(System.in);
-		String s=sc.nextLine();
-        
-		String reverse=reverseString(s);
-		
-		System.out.println(reverse);
-		
-	}
+/** this is the reverse of a string**/
+final class Solution {
+       /**this is a program**/
+       private Solution() { }
+       /**@param args this is a string**/
+    public static void main(final String[] args) {
+        /** this is the string**/
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String reverse = reverseString(s);
+        System.out.println(reverse);
+    }
     
-	static String reverseString(String reverse1) {//hellowrld
-		String s ="";
-		for (int i = reverse1.length()-1; i>=0 ; i--) {
-			//System.out.println(reverse.charAt(i));
-			s = s + reverse1.charAt(i);
-		}
-		return s;
-	}
+    static String reverseString(String reverse1) {//hellowrld
+        String s = "";
+        for (int i = reverse1.length()-1; i >= 0 ; i--) {
+            //System.out.println(reverse.charAt(i));
+            s = s + reverse1.charAt(i);
+        }
+        return s;
+    }
 
 
 }
