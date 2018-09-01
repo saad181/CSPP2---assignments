abb88d4247874ee7c3b0fe199ae7e1b0c6006828
saad181 <saadahmed181@msitprogram.net>
2018-09-01 10:35:25 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- m6 exam/Assignment-1/Solution.java
+++ m6 exam/Assignment-1/Solution.java
@@ -18,14 +18,13 @@ public final class Solution {
      * @param      n     n value
      */
     static void oddComposites(final int n) {
-	for (int i = 2; i <= n; i++) {
+    for (int i = 2; i <= n; i++) {
         for (int j = 2; j < i; j++) {
             if (i % j == 0) {
                 if (i % 2 != 0) {
                    System.out.println(i);
                    break;
                 }
-                
             }
         }
     }
