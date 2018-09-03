dbcd6931a2e3491e1aa8b5c8ea651d822499955e
saad181 <saadahmed181@msitprogram.net>
2018-09-03 19:13:41 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m7/Assignment-1/Solution.java
+++ m7/Assignment-1/Solution.java
@@ -7,22 +7,23 @@ class InputValidator
 		 data = da;
         length = da.length();
 	}
-	
-  public boolean validateData() {
+	public boolean validateData() {
 		if (length >= 6) {
 			return true;
 		}
-		else 
+		else {
 			return false;
-		}	
+		}
+		}
 		}
 public class Solution
 {
-	public static void main(String args[])
+	public static void main(String [] args)
+	/** this is the main method**/
     {
     	Scanner s = new Scanner(System.in);
     	String input = s.next();
-    	InputValidator i = new InputValidator(input);    	
+    	InputValidator i = new InputValidator(input);
     	System.out.println(i.validateData());
     }
 }
