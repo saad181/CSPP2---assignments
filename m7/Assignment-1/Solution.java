7c3368cf633731f63f9dc61c3d56650785aca51d
saad181 <saadahmed181@msitprogram.net>
2018-09-03 19:23:50 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m7/Assignment-1/Solution.java
+++ m7/Assignment-1/Solution.java
@@ -1,30 +1,35 @@
 import java.util.Scanner;
 /** this is the length of the string.**/
-class InputValidator
-{
-	String data;
-	int length;
-	InputValidator(final String da) {
-		 data = da;
+class InputValidator {
+    String data;
+    private int length;
+
+    /**
+     * Constructs the object.
+     *
+     * @param      da    { for input. }
+     */
+    InputValidator(final String da) {
+         data = da;
         length = da.length();
-	}
-	public boolean validateData() {
-		int six = 6;
-		if (length >= six) {
-			return true;
-		}
-		else {
-			return false;
-		}
-		}
-		}
+    }
+    public boolean validateData() {
+        int six = 6;
+        if (length >= six) {
+            return true;
+        }
+        else {
+            return false;
+        }
+        }
+        }
 public class Solution
 {
-	public static void main(final String [] args) {
-	/** this is the main method.**/
-    	Scanner s = new Scanner(System.in);
-    	String input = s.next();
-    	InputValidator i = new InputValidator(input);
-    	System.out.println(i.validateData());
+    public static void main(final String[] args) {
+    /** this is the main method.**/
+        Scanner s = new Scanner(System.in);
+        String input = s.next();
+        InputValidator i = new InputValidator(input);
+        System.out.println(i.validateData());
     }
 }
