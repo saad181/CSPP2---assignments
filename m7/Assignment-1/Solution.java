2c32bb7e722374d23761252f53c133e717183209
saad181 <saadahmed181@msitprogram.net>
2018-09-03 19:06:29 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

---
+++ m7/Assignment-1/Solution.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class InputValidator
+{
+	String data;
+	int length;
+	InputValidator(String da) {
+		 data = da;
+        length = da.length();
+	}
+	
+  public boolean validateData() {
+		if (length >= 6) {
+			return true;
+		}
+		else {
+			return false;
+		}	
+		}
+	}
+
+public class Solution
+{
+	public static void main(String args[])
+    {
+    	Scanner s=new Scanner(System.in);
+    	String input=s.next();
+    	InputValidator i=new InputValidator(input);    	
+    	System.out.println(i.validateData());
+
+    }
+
+}
