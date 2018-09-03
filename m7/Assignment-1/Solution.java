477f690ec2556bb11f44f29da39df57624dbacdb
saad181 <saadahmed181@msitprogram.net>
2018-09-03 19:47:53 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m7/Assignment-1/Solution.java
+++ m7/Assignment-1/Solution.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 /**
 this is the length of the string.
-*/
+**/
 class InputValidator {
     private String data;
     private int length;
@@ -9,33 +9,35 @@ class InputValidator {
      * Constructs the object.
      *
      * @param      da    { for input. }
-     */
+     **/
     InputValidator(final String da) {
          data = da;
         length = da.length();
     }
     /**
-     * { function_description }
+     * function_description.
      *
      * @return     { it returns the true or false. }
-     */
+     **/
     public boolean validateData() {
 
-        int six = 6;
+        final int six = 6;
         if (length >= six) {
             return true;
         }
-        else {
-            return false;
-        }
+        return false;
     }
 }
+/**
+ * Class for solution.
+ **/
 public class Solution {
+    private Solution() { }
     /**
      * { args is for arguments. }
      *
      * @param      args  The arguments
-     */
+     **/
     public static void main(final String[] args) {
     /** this is the main method.**/
         Scanner s = new Scanner(System.in);
