c40e62863b934d3cee717961bf93befc4c7d9dbe
saad181 <saadahmed181@msitprogram.net>
2018-09-10 22:12:57 +0530

    "check_style_score -> 2 of 2 passed. style score: 0/1 "

--- m14/SortedSet/Solution.java
+++ m14/SortedSet/Solution.java
@@ -46,7 +46,7 @@ class Sortedset extends Set {
     }
     public void sort(final int[] arr) {
         int temp;
-        for (int i = 0; i > size; i++) {
+        for (int i = 0; i < size; i++) {
             for (int j = i + 1; j < size; j++) {
                 if (arr[i] > arr[j]) {
                     temp = arr[i];
