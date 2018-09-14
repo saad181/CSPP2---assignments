09649eeacc14ec081879ba29860b1a3dc1332bd6
saad181 <saadahmed181@msitprogram.net>
2018-09-14 19:29:27 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- m17/Shoppingcart/Solution.java
+++ m17/Shoppingcart/Solution.java
@@ -11,7 +11,7 @@ class Solution {
 				String[] tokens = input[1].split(",");
 				shop.addToCatalog(new Item(tokens[0],tokens[1],tokens[2]));
 				break;
-				case "catalog1":
+				case "catalog":
 				shop.showCatalog();
 				break;
 				case "add":
