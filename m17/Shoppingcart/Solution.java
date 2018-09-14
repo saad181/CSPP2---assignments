eccc2f57a8a2d5e311a56358ac14ccefb61cfe5a
saad181 <saadahmed181@msitprogram.net>
2018-09-14 19:27:36 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ m17/Shoppingcart/Solution.java
@@ -0,0 +1,45 @@
+import java.util.*;
+class Solution {
+	public static void main(String[] args) {
+		ShoppingCart shop = new ShoppingCart();
+		Scanner sc = new Scanner(System.in);
+		int lines = Integer.parseInt(sc.nextLine());
+		for(int i =0;i< lines;i++){
+			String[] input = sc.nextLine().split(" ");
+			switch(input[0]){
+				case "Item1":
+				String[] tokens = input[1].split(",");
+				shop.addToCatalog(new Item(tokens[0],tokens[1],tokens[2]));
+				break;
+				case "catalog":
+				shop.showCatalog();
+				break;
+				case "add":
+				String[] tokens1 = input[1].split(",");
+				shop.addToCart(new Item(tokens1[0],tokens1[1]));
+				break;
+				case "show":
+				shop.showCart();
+				break;
+				case "totalAmount":
+				System.out.println("totalAmount: "+ shop.getTotalAmount());
+				break;
+				case "payableAmount":
+				System.out.println("Payable amount: "+ shop.getPayableAmount());
+				break;
+				case "remove":
+				String[] tokens12 = input[1].split(",");
+				shop.removeFromCart(new Item(tokens12[0],tokens12[1]));
+				break;
+				case "coupon":
+				shop.applyCoupon(input[1]);
+				break;
+				case "print":
+				shop.printInvoice();
+				break;
+			}
+		}
+
+	}
+
+}
