67b0942dcfdb102d2f908cccb5ee7369f8051c5d
saad181 <saadahmed181@msitprogram.net>
2018-09-22 17:09:23 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- m24/assignment 2/Stringmatching/Solution.java
+++ m24/assignment 2/Stringmatching/Solution.java
@@ -1,40 +1,40 @@
 import java.util.*;
 import java.io.*;
 class Solution {
-	public static void main(String[] args) {
-		try {
-			File test1 = new File("File1.txt");
-			Scanner s = new Scanner(test1);
-			String s1 = "";
-			while(s.hasNext()) {
-				s1 += s.nextLine();
+    public static void main(final String[] args) {
+        try {
+            File test1 = new File("File1.txt");
+            Scanner s = new Scanner(test1);
+            String s1 = "";
+            while (s.hasNext()) {
+                s1 += s.nextLine();
 
-			}
-			File test2 = new File("File2.txt");
-			s = new Scanner(test2);
-			String s2 = "";
-			while(s.hasNext()) {
-				s2 += s.nextLine();
-			}
-			String lcs = "";
-			for (int i = 0; i < s2.length(); i++) {
-				for (int j = i+1; j <= s2.length(); j++) {
-					if (s1.contains(s2.substring(i,j))) {
-						if (s2.substring(i, j).length() > lcs.length()) {
-							lcs = s2.substring(i, j).replaceAll(" ","");
-						}
-					}
-				}	
-			}
-			System.out.println(lcs + "\tand its length is\t" + lcs.length());
-			int numerator = lcs.length() *2 * 100;
-			System.out.println(numerator);
-			int denominator = s1.length() + s2.length();
-			System.out.println(denominator);
-			System.out.println((numerator/denominator));
-		} catch (Exception e) {
-			System.out.println("FILE NOT FOUND");
-		}	
+            }
+            File test2 = new File("File2.txt");
+            s = new Scanner(test2);
+            String s2 = "";
+            while (s.hasNext()) {
+                s2 += s.nextLine();
+            }
+            String lcs = "";
+            for (int i = 0; i < s2.length(); i++) {
+                for (int j = i + 1; j <= s2.length(); j++) {
+                    if (s1.contains(s2.substring(i, j))) {
+                        if (s2.substring(i, j).length() > lcs.length()) {
+                            lcs = s2.substring(i, j).replaceAll(" ","");
+                        }
+                    }
+                }
+            }
+            System.out.println(lcs + "\tand its length is\t" + lcs.length());
+            int numerator = lcs.length() * 2 * 100;
+            System.out.println(numerator);
+            int denominator = s1.length() + s2.length();
+            System.out.println(denominator);
+            System.out.println((numerator / denominator));
+        } catch (Exception e) {
+            System.out.println("FILE NOT FOUND");
+        }
     }
-}		
-	
+}
+    
