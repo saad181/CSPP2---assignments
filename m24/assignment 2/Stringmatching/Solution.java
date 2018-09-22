2a44cedd3297880fdd3a08df4b71a41018c51cba
saad181 <saadahmed181@msitprogram.net>
2018-09-22 17:16:02 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- m24/assignment 2/Stringmatching/Solution.java
+++ m24/assignment 2/Stringmatching/Solution.java
@@ -29,7 +29,7 @@ class Solution {
                 for (int j = i + 1; j <= s2.length(); j++) {
                     if (s1.contains(s2.substring(i, j))) {
                         if (s2.substring(i, j).length() > lcs.length()) {
-                            lcs = s2.substring(i, j).replaceAll(" ","");
+                            lcs = s2.substring(i, j).replaceAll(" ", "");
                         }
                     }
                 }
